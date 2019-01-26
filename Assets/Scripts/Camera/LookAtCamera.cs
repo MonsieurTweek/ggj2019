ceac74049c19d565cea3bb7d8e50bfb91fed8cba
MonsieurTweek <gaetan.regent@gmail.com>
2019-01-26 04:00:28 +0100

    Main Scene + Main Camera

---
+++ Assets/Scripts/Camera/LookAtCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+
+    public GameObject target;
+    public float damping = 1;
+
+    private Vector3 _offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + _offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(target.transform.position);
+    }
+}

bf5dface626271b053eaec5844e11b1dfe434f7d
MonsieurTweek <gaetan.regent@gmail.com>
2019-01-26 09:52:26 +0100

    Die & Retry (A)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public PlayerController player;
+    public GameObject spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        player.Reset();
+        player.transform.position = spawner.transform.position;
+    }
+}
